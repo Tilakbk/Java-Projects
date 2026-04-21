90eb82f9b460cf252c8833c6169d65a0639fdea6
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-04-21 10:07:14 +0545

    getter and constructor initaoted

--- CoreJava/LMS/Library/User.java
+++ CoreJava/LMS/Library/User.java
@@ -1,5 +1,41 @@
 package CoreJava.LMS.Library;
 
-public class User {
+public class User
+{
+    protected String name;
+    protected String email;
+    protected String phoneNumber;
     
+    public User()
+    {
+         name= null;
+         email=null;
+         phoneNumber=null;
+    }
+
+    public User(String name)
+    {
+        this.name=name;
+    }
+    public User(String name,String email,String phoneNumber)
+    {
+        this.name=name;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
 }
