7afc6adc9d93ae0b45f07f3fe7449f181915779a
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-04-21 13:12:22 +0545

    Login credential verificatio added

--- CoreJava/LMS/Library/Database.java
+++ CoreJava/LMS/Library/Database.java
@@ -13,6 +13,19 @@ public class Database
         usernames.add(s.getName());
     }
     
+public boolean login(String phoneNumber, String email)
+{
+    boolean n= false;
+    for(User s: users)
+    {
+        if(s.getPhoneNumber().matches(phoneNumber)&&s.getEmail().matches(email))
+        {
+            n=true;
+            break;
+        }
+    }
 
+    return n;
+}
 
 }
