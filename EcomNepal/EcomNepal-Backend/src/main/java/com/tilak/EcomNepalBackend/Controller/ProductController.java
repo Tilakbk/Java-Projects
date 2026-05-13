b5db2082dc735749ccf94db2a78d3115017bc459
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-13 09:04:48 +0545

    Controller checked

--- EcomNepal/EcomNepal-Backend/src/main/java/com/tilak/EcomNepalBackend/Controller/ProductController.java
+++ EcomNepal/EcomNepal-Backend/src/main/java/com/tilak/EcomNepalBackend/Controller/ProductController.java
@@ -1,5 +1,16 @@
 package com.tilak.EcomNepalBackend.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
-    
+
+    @GetMapping("/")
+    public String greet()
+    {
+        return "Hello";
+    }
 }
