6323ceeb8d5d519b19b2f8adff8ba5278f9ef589
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-13 12:34:04 +0545

    Front and bakend cross origin resource sharing error resolve

--- EcomNepal/EcomNepal-Backend/src/main/java/com/tilak/EcomNepalBackend/Controller/ProductController.java
+++ EcomNepal/EcomNepal-Backend/src/main/java/com/tilak/EcomNepalBackend/Controller/ProductController.java
@@ -3,15 +3,13 @@ package com.tilak.EcomNepalBackend.Controller;
 import com.tilak.EcomNepalBackend.Model.Product;
 import com.tilak.EcomNepalBackend.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
