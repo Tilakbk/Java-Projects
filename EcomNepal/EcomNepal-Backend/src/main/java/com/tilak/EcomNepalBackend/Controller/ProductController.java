9d0aea419322b9d3fc035b59c97becdad0e646c7
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-13 08:58:36 +0545

    product and controller added using lombol for getter setter

---
+++ EcomNepal/EcomNepal-Backend/src/main/java/com/tilak/EcomNepalBackend/Controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.tilak.EcomNepalBackend.Controller;
+
+public class ProductController {
+    
+}
---
+++ EcomNepal/EcomNepal-Backend/src/main/java/com/tilak/EcomNepalBackend/Model/Product.java
@@ -0,0 +1,25 @@
+package com.tilak.EcomNepalBackend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Data
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private int price;
+    private String category;
+    private Date releasedate;
+    private boolean available;
+    private int quantity;
+
+}
