2175400810adbe8104905bc0af344f525c375dde
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-19 09:35:24 +0545

    project setup added

---
+++ RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/RojgarNepalApplication.java
@@ -0,0 +1,13 @@
+package com.tilak.RojgarNepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojgarNepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RojgarNepalApplication.class, args);
+	}
+
+}
---
+++ RojgarNepal/RojgarNepal-Backend/src/test/java/com/tilak/RojgarNepal/RojgarNepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tilak.RojgarNepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RojgarNepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
