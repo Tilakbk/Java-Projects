52e9665570f3fcb65df6881f04fb09b55756ce07
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-19 12:28:51 +0545

    Swagger configuration file added

---
+++ RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/Controller/JobController.java
@@ -0,0 +1,17 @@
+package com.tilak.RojgarNepal.Controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class JobController
+{
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+}
