27204a295c45f6c98d2c82c557d2034db310be76
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-19 18:45:54 +0545

    Atlas search is implemented through aggregated pipeline

--- RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/Repos/SearchInterfaceImplementation.java
+++ RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/Repos/SearchInterfaceImplementation.java
@@ -1,16 +1,60 @@
 package com.tilak.RojgarNepal.Repos;
 
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.tilak.RojgarNepal.Model.Post;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+//import java.util.List;
+//import java.util.Arrays;
+//import org.bson.Document;
+//import com.mongodb.MongoClient;
+//import com.mongodb.MongoClientURI;
+//import com.mongodb.client.MongoCollection;
+//import com.mongodb.client.MongoDatabase;
+//import org.bson.conversions.Bson;
+//import java.util.concurrent.TimeUnit;
+//import org.bson.Document;
+//import com.mongodb.client.AggregateIterable;
 
 @Component
 public class SearchInterfaceImplementation implements SearchRepo{
+
+    @Autowired
+    MongoClient mongoClient;
+
+    @Autowired
+    MongoConverter converter;
+
     @Override
     public List<Post> search(String query)
     {
 
-        return null;
+        final List<Post> posts= new ArrayList<>();
+
+        MongoDatabase database = mongoClient.getDatabase("RojgarNepal");
+        MongoCollection<Document> collection = database.getCollection("Jobpost");
+        AggregateIterable<Document> result = collection.aggregate(Arrays.asList(new Document("$search",
+                        new Document("index", "default")
+                                .append("text",
+                                        new Document("query", query)
+                                                .append("path", Arrays.asList("desc", "profile", "techs"))
+                                                .append("matchCriteria", "any"))),
+                new Document("$sort",
+                        new Document("exp", 1L)),
+                new Document("$limit", 4L)));
+
+        result.forEach(doc-> posts.add(converter.read(Post.class,doc)));
+
+        return posts;
     }
 }
