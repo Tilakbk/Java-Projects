54619842eaf2a7ddbff20d278514f6878c81946c
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-19 13:08:05 +0545

    Post mapping for all post created

--- RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/Controller/JobController.java
+++ RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/Controller/JobController.java
@@ -1,17 +1,27 @@
 package com.tilak.RojgarNepal.Controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.io.IOException;
 
 @RestController
 public class JobController
 {
+    @Hidden
     @RequestMapping(value = "/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
     }
 
+    @PostMapping("/allPosts")
+    public ResponseEntity<String> getAllPosts()
+    {
+
+    }
 }
---
+++ RojgarNepal/RojgarNepal-Backend/src/main/java/com/tilak/RojgarNepal/Service/JobService.java
@@ -0,0 +1,5 @@
+package com.tilak.RojgarNepal.Service;
+
+
+public class JobService {
+}
