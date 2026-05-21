9a3ec296a1445187352f3af4af7f16a98d59d55e
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-21 14:48:19 +0545

    ollama controller cross origin added

--- AskMeApp/AskMe-Backend/src/main/java/com/tilak/AskMe/Ollama/OllamaController.java
+++ AskMeApp/AskMe-Backend/src/main/java/com/tilak/AskMe/Ollama/OllamaController.java
@@ -1,16 +1,13 @@
 package com.tilak.AskMe.Ollama;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:5173/")
 @RequestMapping("/api/ollama")
 public class OllamaController {
 
