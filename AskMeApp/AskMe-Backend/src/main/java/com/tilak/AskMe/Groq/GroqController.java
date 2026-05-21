3b7a5ea779303737c83784523b321a215b41cd63
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-21 12:20:40 +0545

    Groq chat mapping completed

--- AskMeApp/AskMe-Backend/src/main/java/com/tilak/AskMe/Groq/GroqController.java
+++ AskMeApp/AskMe-Backend/src/main/java/com/tilak/AskMe/Groq/GroqController.java
@@ -1,9 +1,27 @@
 package com.tilak.AskMe.Groq;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/openai")
+@CrossOrigin("http://localhost:5173/")
 public class GroqController {
 
+    private final OpenAiChatModel chatModel;
+
+    public GroqController( OpenAiChatModel chatModel)
+    {
+        this.chatModel=chatModel;
+    }
+
+    @GetMapping("/{query}")
+    public ResponseEntity<String> groqResponse(@PathVariable String query)
+    {
+        String response= chatModel.call(query);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
