e7b1a1f34d826fe81b72e84b1fdc401cf2cf901d
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-21 14:19:41 +0545

    ollama controller implemented

--- AskMeApp/AskMe-Backend/src/main/java/com/tilak/AskMe/Ollama/OllamaController.java
+++ AskMeApp/AskMe-Backend/src/main/java/com/tilak/AskMe/Ollama/OllamaController.java
@@ -1,5 +1,12 @@
 package com.tilak.AskMe.Ollama;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/ollama")
 public class OllamaController {
 
+    private final ChatClient chatModel;
+
+    public OllamaController( OllamaChatModel chatModel)
+    {
+        this.chatModel= ChatClient.create(chatModel);
+    }
+
+    @GetMapping("/{query}")
+    public ResponseEntity<String> GenAiResponse(@PathVariable String query)
+    {
+        String response= chatModel.prompt(query).call().content();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
