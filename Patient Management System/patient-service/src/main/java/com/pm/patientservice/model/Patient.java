b89eda108016e566dcc7a2a3afa6b69ec33c90e2
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-24 08:09:30 +0545

    data.sql executed successfully and patient Repository file added

--- Patient Management System/patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ Patient Management System/patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -55,12 +55,12 @@ public class Patient {
         this.email = email;
     }
 
-    public String getAdress() {
+    public String getAddress() {
         return address;
     }
 
-    public void setAdress(String adress) {
-        this.address = adress;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public LocalDate getDateOfBirth() {
