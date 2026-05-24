9279c3c3399bbab23a3a19a8a61b864ca18afcc4
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-05-24 08:22:30 +0545

    service layer and dto layer added with repective file

---
+++ Patient Management System/patient-service/src/main/java/com/pm/patientservice/services/PatientService.java
@@ -0,0 +1,26 @@
+package com.pm.patientservice.services;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository)
+    {
+        this.patientRepository=patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatient()
+    {
+        List<Patient> patients = patientRepository.findAll();
+        return patients;
+    }
+
+}
