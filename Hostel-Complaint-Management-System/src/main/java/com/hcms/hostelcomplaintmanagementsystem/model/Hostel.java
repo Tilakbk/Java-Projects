5c08bab95d5ff7782698a86fef4f07d6f69d7a18
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-02 15:22:53 +0545

    attribute created for hostel and Room entity

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Hostel.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Hostel.java
@@ -1,13 +1,22 @@
 package com.hcms.hostelcomplaintmanagementsystem.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Hostel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID hostel_id;
 }
--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Room.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Room.java
@@ -1,4 +1,30 @@
 package com.hcms.hostelcomplaintmanagementsystem.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID room_id;
+
+    @NotNull
+    private int room_number;
+
+    @NotNull
+    private int capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "hostel_id", nullable = false)
+    private Hostel hostel;
 }
