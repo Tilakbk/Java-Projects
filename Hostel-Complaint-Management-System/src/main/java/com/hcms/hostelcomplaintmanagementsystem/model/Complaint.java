4ba164c63cb1ccfad24105ae03b450b904fefc7a
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-02 16:55:43 +0545

    Complaint and Category model created

---
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Complaint.java
@@ -0,0 +1,49 @@
+package com.hcms.hostelcomplaintmanagementsystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Complaint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID complaint_id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "student_id",nullable = false)
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private Category category;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String status;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+
+
+}
