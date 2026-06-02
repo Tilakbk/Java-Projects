aa432865b1fb1949afe3995b1f17e8c29c1212ae
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-02 11:22:24 +0545

    hostel complaint management system added

---
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/HostelComplaintManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hcms.hostelcomplaintmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelComplaintManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelComplaintManagementSystemApplication.class, args);
+    }
+
+}
---
+++ Hostel-Complaint-Management-System/src/test/java/com/hcms/hostelcomplaintmanagementsystem/HostelComplaintManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcms.hostelcomplaintmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelComplaintManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
