667b7c13b75a77b85a0b2b8740d22013eef6b911
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-03 10:38:36 +0545

    Resolution log  entity attribute added

---
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/ResolutionLog.java
@@ -0,0 +1,40 @@
+package com.hcms.hostelcomplaintmanagementsystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class ResolutionLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID log_id;
+
+    @ManyToOne
+    @JoinColumn(name = "complaint_id",nullable = false)
+    private Complaint complaint;
+
+    @ManyToOne
+    @JoinColumn(name="staff_id",nullable = false)
+    private Staff staff;
+
+    @NotNull
+    private String action_taken;
+
+    @LastModifiedDate
+    private LocalDateTime resolved_at;
+
+
+}
