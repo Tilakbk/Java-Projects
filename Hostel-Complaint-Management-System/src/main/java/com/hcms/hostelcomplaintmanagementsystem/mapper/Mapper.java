0c1cc26b6a1e49c1345918500d93e82d5eb5ff67
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-03 12:36:46 +0545

    Student mapper function created

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/mapper/Mapper.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/mapper/Mapper.java
@@ -1,7 +1,22 @@
 package com.hcms.hostelcomplaintmanagementsystem.mapper;
 
+import com.hcms.hostelcomplaintmanagementsystem.dto.StudentResponseDto;
+import com.hcms.hostelcomplaintmanagementsystem.model.Student;
+
 public class Mapper {
 
+    public static StudentResponseDto toStudentResponseDto(Student student)
+    {
+        StudentResponseDto studentResponseDto= new StudentResponseDto();
 
+        studentResponseDto.setName(student.getName());
+        studentResponseDto.setEmail(student.getEmail());
+        studentResponseDto.setPhone(student.getPhone());
+        studentResponseDto.setRoom_number(student.getRoom().getRoom_number());
+        studentResponseDto.setHostel_name(student.getHostel().getName());
+
+        return studentResponseDto;
+
+    }
 
 }
