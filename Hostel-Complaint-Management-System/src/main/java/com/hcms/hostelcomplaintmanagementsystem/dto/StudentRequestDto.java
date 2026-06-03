ed254a569f759798e0e20d7da6a805efdfd6a11e
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-03 15:02:18 +0545

    student request dto created

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/StudentRequestDto.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/StudentRequestDto.java
@@ -1,4 +1,20 @@
 package com.hcms.hostelcomplaintmanagementsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentRequestDto {
+
+    private String name;
+    private String email;
+    private String phone;
+    private UUID room_id;
+    private UUID hostel_id;
+
 }
--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/mapper/Mapper.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/mapper/Mapper.java
@@ -1,9 +1,16 @@
 package com.hcms.hostelcomplaintmanagementsystem.mapper;
 
+import com.hcms.hostelcomplaintmanagementsystem.dto.StudentRequestDto;
 import com.hcms.hostelcomplaintmanagementsystem.dto.StudentResponseDto;
+import com.hcms.hostelcomplaintmanagementsystem.model.Hostel;
 import com.hcms.hostelcomplaintmanagementsystem.model.Student;
+import com.hcms.hostelcomplaintmanagementsystem.repository.HostelRepo;
+import com.hcms.hostelcomplaintmanagementsystem.repository.RoomRepo;
+import com.hcms.hostelcomplaintmanagementsystem.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Mapper {
+
 
     public static StudentResponseDto toStudentResponseDto(Student student)
     {
@@ -16,6 +23,16 @@ public class Mapper {
         studentResponseDto.setHostel_name(student.getHostel().getName());
 
         return studentResponseDto;
+
+    }
+
+    public static Student toStudent(StudentRequestDto studentRequestDto)
+    {
+        Student student= new Student();
+
+        student.setName(studentRequestDto.getName());
+        student.setEmail(studentRequestDto.getEmail());
+        student.setPhone(studentRequestDto.getPhone());
 
     }
 
