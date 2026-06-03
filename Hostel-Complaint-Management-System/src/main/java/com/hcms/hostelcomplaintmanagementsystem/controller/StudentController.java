b2619b4162036bc3d4857ad336f079da3400f668
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-03 12:53:54 +0545

    Student service and controller get request mapping function added

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/StudentController.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class StudentController {
@@ -21,7 +23,7 @@ public class StudentController {
     @GetMapping("/students")
     private ResponseEntity<List<StudentResponseDto>> getAllStudent()
     {
-
+        return ResponseEntity.ok().body(studentService.getAllStudent());
     }
     
 }
--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/service/StudentService.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/service/StudentService.java
@@ -1,9 +1,33 @@
 package com.hcms.hostelcomplaintmanagementsystem.service;
 
+import com.hcms.hostelcomplaintmanagementsystem.dto.StudentResponseDto;
+import com.hcms.hostelcomplaintmanagementsystem.mapper.Mapper;
+import com.hcms.hostelcomplaintmanagementsystem.model.Student;
+import com.hcms.hostelcomplaintmanagementsystem.repository.StudentRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class StudentService {
 
+    private StudentRepo studentRepo;
+
+    public StudentService(StudentRepo studentRepo)
+    {
+        this.studentRepo=studentRepo;
+    }
+
+    public List<StudentResponseDto> getAllStudent()
+    {
+        List<Student> students= studentRepo.findAll();
+        Map<String,String> response = new HashMap<>();
+        return students.stream()
+                .map(Mapper::toStudentResponseDto)
+                .toList();
+
+    }
 
 }
