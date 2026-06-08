8cd57e0764fc0127c41e3f423ada67bd1b6bbe8c
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-08 14:17:44 +0545

    Resolution log request dto field added

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/ResolutionLogRequestDto.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/ResolutionLogRequestDto.java
@@ -1,7 +1,22 @@
 package com.hcms.hostelcomplaintmanagementsystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 public class ResolutionLogRequestDto {
+
+    @NotNull(message = "Complaint id is required")
+    private UUID complaintId;
+
+    @NotNull(message = "Staff id is required")
+    private UUID staffId;
+
+    @NotBlank(message = "Action is required")
+    private String actionTaken;
+
+
 }
