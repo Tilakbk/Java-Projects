679c6484ef06144a40eeb7aad04928d0d670a174
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-09 10:19:41 +0545

    Category_name changed to CategoryName  done

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/CategoryResponseDto.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/CategoryResponseDto.java
@@ -1,6 +1,9 @@
 package com.hcms.hostelcomplaintmanagementsystem.dto;
 
+import java.util.UUID;
+
 public class CategoryResponseDto {
 
-
+    private UUID categoryId;
+    private String categoryName;
 }
--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Category.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/model/Category.java
@@ -18,5 +18,5 @@ public class Category {
     private UUID categoryId;
 
     @NotNull
-    private String category_name;
+    private String categoryName;
 }
