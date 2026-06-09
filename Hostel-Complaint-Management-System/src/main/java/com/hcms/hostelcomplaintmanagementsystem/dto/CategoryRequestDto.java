456192a030691630a990c7b48269e14c18118dde
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-09 10:20:41 +0545

    @Data added in both dto

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/CategoryRequestDto.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/CategoryRequestDto.java
@@ -1,7 +1,9 @@
 package com.hcms.hostelcomplaintmanagementsystem.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class CategoryRequestDto {
 
     @NotBlank(message = "Name of the category is mandatory")
--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/CategoryResponseDto.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/dto/CategoryResponseDto.java
@@ -1,7 +1,10 @@
 package com.hcms.hostelcomplaintmanagementsystem.dto;
 
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
 public class CategoryResponseDto {
 
     private UUID categoryId;
