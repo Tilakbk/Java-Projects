55088f8e0a378439127c191c64bfff54a761479f
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-09 10:04:22 +0545

    Complaint category service DI done

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/service/CategoryService.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/service/CategoryService.java
@@ -1,9 +1,14 @@
 package com.hcms.hostelcomplaintmanagementsystem.service;
 
+import com.hcms.hostelcomplaintmanagementsystem.repository.CategoryRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
 
+    private final CategoryRepo categoryRepo;
 
+    public CategoryService(CategoryRepo categoryRepo) {
+        this.categoryRepo = categoryRepo;
+    }
 }
