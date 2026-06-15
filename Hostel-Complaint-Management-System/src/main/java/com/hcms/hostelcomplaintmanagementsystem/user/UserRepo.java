8c3409b0ee0dff8cd8df4607f629cbc63748d962
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-15 11:20:35 +0545

    User repo created

---
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/user/UserRepo.java
@@ -0,0 +1,10 @@
+package com.hcms.hostelcomplaintmanagementsystem.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, UUID> {
+}
