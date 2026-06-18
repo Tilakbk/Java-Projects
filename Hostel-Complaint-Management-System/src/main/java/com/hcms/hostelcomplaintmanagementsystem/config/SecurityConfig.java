bf27a7bb37571b0ad323b0965092b9ada87fa8da
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-18 11:15:28 +0545

    SecurityConfig class added

---
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.hcms.hostelcomplaintmanagementsystem.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+
+}
