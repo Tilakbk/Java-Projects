d4b96dbdc8320c25023312be06ceb31683db1f6e
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-19 10:41:24 +0545

    Auth dto created

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/auth/RegisterRequestDto.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/auth/RegisterRequestDto.java
@@ -1,5 +1,34 @@
 package com.hcms.hostelcomplaintmanagementsystem.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequestDto {
+
+    @NotBlank(message = "Full name is required")
+    @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+
+
+
 }
