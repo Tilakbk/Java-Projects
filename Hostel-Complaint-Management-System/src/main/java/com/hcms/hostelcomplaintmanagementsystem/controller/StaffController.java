d9437b83aad884d29b03b58a7b5fc5e0562e0f00
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-23 13:36:58 +0545

    Applied method level authentication in staff

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/StaffController.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/StaffController.java
@@ -4,7 +4,9 @@ import com.hcms.hostelcomplaintmanagementsystem.dto.StaffPatchRequestDto;
 import com.hcms.hostelcomplaintmanagementsystem.dto.StaffRequestDto;
 import com.hcms.hostelcomplaintmanagementsystem.dto.StaffResponseDto;
 import com.hcms.hostelcomplaintmanagementsystem.service.StaffService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,51 +15,55 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class StaffController {
 
     private final StaffService staffService;
 
-    public StaffController(StaffService staffService) {
-        this.staffService = staffService;
-    }
-
     @GetMapping("/staff")
+    @PreAuthorize("hasAnyRole('WARDEN', 'SYSTEM_ADMIN')")
     public ResponseEntity<List<StaffResponseDto>> getAllStaff()
     {
         return ResponseEntity.ok().body(staffService.getAllStaff());
     }
 
     @PostMapping("/staff")
+    @PreAuthorize("hasRole('WARDEN')")
     public ResponseEntity<StaffResponseDto> addStaff(@Validated @RequestBody StaffRequestDto staffRequestDto)
     {
         return ResponseEntity.ok().body(staffService.addStaff(staffRequestDto));
     }
 
     @GetMapping("/staff/{id}")
+    @PreAuthorize("hasAnyRole('WARDEN', 'SYSTEM_ADMIN') or @securityService.isStaffOwner(#id, authentication)")
     public ResponseEntity<StaffResponseDto> getStaffById(@PathVariable UUID id)
     {
         return ResponseEntity.ok(staffService.getStaffById(id));
     }
 
     @GetMapping("/staff/role/{role}")
+    @PreAuthorize("hasAnyRole('WARDEN', 'SYSTEM_ADMIN')")
     public ResponseEntity<List<StaffResponseDto>> getStaffByRole(@PathVariable String role)
     {
         return ResponseEntity.ok(staffService.getStaffByRole(role));
     }
 
     @PutMapping("/staff/{id}")
+    @PreAuthorize("hasRole('WARDEN')")
     public ResponseEntity<StaffResponseDto> updateStaffById(@PathVariable UUID id, @Validated @RequestBody StaffRequestDto staffRequestDto)
     {
         return ResponseEntity.ok(staffService.updateStaffById(id,staffRequestDto));
     }
 
     @PatchMapping("/staff/{id}")
+    @PreAuthorize("hasRole('WARDEN')")
     public ResponseEntity<StaffResponseDto> partiallyUpdateStaffById(@PathVariable UUID id, @Validated @RequestBody StaffPatchRequestDto staffPatchRequestDto)
     {
         return ResponseEntity.ok(staffService.partiallyUpdateStaffById(id,staffPatchRequestDto));
     }
 
     @DeleteMapping("/staff/{id}")
+    @PreAuthorize("hasRole('WARDEN')")
     public ResponseEntity<?> deleteStaff(@PathVariable UUID id)
     {
         staffService.deleteStaff(id);
