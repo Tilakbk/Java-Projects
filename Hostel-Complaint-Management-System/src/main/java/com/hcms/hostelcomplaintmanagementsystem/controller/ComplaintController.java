852636fd00c6b1ce9b31f621f694738cdf4f7e48
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-06-23 13:19:29 +0545

    PreAuthorize annotation added in complaints and hostel

--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/ComplaintController.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/ComplaintController.java
@@ -5,7 +5,9 @@ import com.hcms.hostelcomplaintmanagementsystem.dto.ComplaintRequestDto;
 import com.hcms.hostelcomplaintmanagementsystem.dto.ComplaintResponseDto;
 import com.hcms.hostelcomplaintmanagementsystem.service.ComplaintService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,51 +16,57 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class ComplaintController {
 
     private final ComplaintService complaintService;
 
-    public ComplaintController(ComplaintService complaintService) {
-        this.complaintService = complaintService;
-    }
-
     @PostMapping("/complaint")
+    @PreAuthorize("hasRole('STUDENT')")
     public ResponseEntity<ComplaintResponseDto> addComplaint(@Validated @RequestBody ComplaintRequestDto complaintRequestDto)
     {
         return ResponseEntity.ok(complaintService.addComplaint(complaintRequestDto));
     }
 
     @GetMapping("/complaint")
+    @PreAuthorize("hasAnyRole('WARDEN','SYSTEM_ADMIN')")
     public ResponseEntity<List<ComplaintResponseDto>> getAllComplaint()
     {
         return ResponseEntity.ok(complaintService.getAllComplaint());
     }
 
     @GetMapping("/complaint/{id}")
+    @PreAuthorize("hasAnyRole('WARDEN', 'SYSTEM_ADMIN') or " +
+            "@securityService.isComplaintOwner(#id, authentication) or " +
+            "@securityService.isAssignedStaff(#id, authentication)")
     public ResponseEntity<ComplaintResponseDto> getComplaintById(@PathVariable UUID id)
     {
         return ResponseEntity.ok(complaintService.getComplaintById(id));
     }
 
     @GetMapping("/complaint/status/{status}")
+    @PreAuthorize("hasAnyRole('WARDEN', 'SYSTEM_ADMIN', 'STAFF')")
     public ResponseEntity<List<ComplaintResponseDto>> getComplaintByStatus(@PathVariable String status)
     {
         return ResponseEntity.ok(complaintService.getComplaintByStatus(status));
     }
 
     @GetMapping("/complaint/student/{studentId}")
+    @PreAuthorize("hasAnyRole('WARDEN', 'SYSTEM_ADMIN') or @securityService.isStudentOwner(#studentId, authentication)")
     public ResponseEntity<List<ComplaintResponseDto>> getComplaintByStudent(@PathVariable UUID studentId)
     {
         return ResponseEntity.ok(complaintService.getComplaintByStudent(studentId));
     }
 
     @PutMapping("/complaint/{id}")
+    @PreAuthorize("hasRole('WARDEN') or @securityService.isComplaintOwner(#id, authentication)")
     public ResponseEntity<ComplaintResponseDto> updateComplaint(@PathVariable UUID id,@RequestBody ComplaintRequestDto complaintRequestDto)
     {
         return ResponseEntity.ok(complaintService.updateComplaint(id,complaintRequestDto));
     }
 
     @DeleteMapping("/complaint/{id}")
+    @PreAuthorize("hasRole('WARDEN')")
     public ResponseEntity<String> deleteComplaint(@PathVariable UUID id)
     {
         complaintService.deleteComplaint(id);
@@ -66,18 +74,21 @@ public class ComplaintController {
     }
 
     @PutMapping("/complaint/{complaintId}/staff/{staffId}")
+    @PreAuthorize("hasRole('WARDEN')")
     public ResponseEntity<ComplaintAssignResponseDto> assignStaffToComplaint(@PathVariable UUID complaintId, @PathVariable UUID staffId)
     {
         return ResponseEntity.ok(complaintService.assignStaffToComplaint(complaintId,staffId));
     }
 
     @GetMapping("/complaint/staff/{staffId}")
+    @PreAuthorize("hasRole('WARDEN') or @securityService.isStaffOwner(#staffId, authentication)")
     public ResponseEntity<List<ComplaintResponseDto>> getComplaintByStaff(@PathVariable UUID staffId)
     {
         return ResponseEntity.ok(complaintService.getComplaintByStaff(staffId));
     }
 
     @PutMapping("/complaint/{id}/resolve")
+    @PreAuthorize("hasRole('STAFF') and @securityService.isAssignedStaff(#id, authentication)")
     public ResponseEntity<ComplaintResponseDto> markComplaintResolved(@PathVariable UUID id,@RequestParam String actionTaken, @RequestParam UUID staffId)
     {
         return ResponseEntity.ok(complaintService.markComplaintResolved(id,actionTaken,staffId));
--- Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/HostelController.java
+++ Hostel-Complaint-Management-System/src/main/java/com/hcms/hostelcomplaintmanagementsystem/controller/HostelController.java
@@ -4,7 +4,9 @@ import com.hcms.hostelcomplaintmanagementsystem.dto.HostelRequestDto;
 import com.hcms.hostelcomplaintmanagementsystem.dto.HostelResponseDTO;
 import com.hcms.hostelcomplaintmanagementsystem.dto.RoomResponseDto;
 import com.hcms.hostelcomplaintmanagementsystem.service.HostelService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,27 +15,29 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class HostelController {
 
     private final HostelService hostelService;
 
-    public HostelController(HostelService hostelService) {
-        this.hostelService = hostelService;
-    }
+
 
     @GetMapping("/hostel")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<HostelResponseDTO>> getAllHostel()
     {
         return ResponseEntity.ok(hostelService.getAllHostel());
     }
 
     @PostMapping("/hostel")
+    @PreAuthorize("hasRole('SYSTEM_ADMIN')")
     public ResponseEntity<HostelResponseDTO> addHostel(@Validated @RequestBody HostelRequestDto hostelRequestDto)
     {
         return ResponseEntity.ok(hostelService.addHostel(hostelRequestDto));
     }
 
     @GetMapping("/hostel/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<HostelResponseDTO> getHostelById(@PathVariable UUID id)
     {
         return ResponseEntity.ok(hostelService.getHostelById(id));
@@ -41,18 +45,21 @@ public class HostelController {
 
 
     @GetMapping("/hostel/{id}/room")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<RoomResponseDto>> getHostelRoomById(@PathVariable UUID id)
     {
         return ResponseEntity.ok(hostelService.getHostelRoomById(id));
     }
 
     @PutMapping("/hostel/{id}")
+    @PreAuthorize("hasRole('SYSTEM_ADMIN')")
     public ResponseEntity<HostelResponseDTO> updateHostel( @RequestBody HostelRequestDto hostelRequestDto, @PathVariable UUID id)
     {
         return ResponseEntity.ok(hostelService.updateHostel(hostelRequestDto,id));
     }
 
     @DeleteMapping("/hostel/{id}")
+    @PreAuthorize("hasRole('SYSTEM_ADMIN')")
     public ResponseEntity<?> deleteHostel(@PathVariable UUID id)
     {
         hostelService.deleteHostel(id);
